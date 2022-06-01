b65dc4e22efaad02107de6bf53d80ad33da9c12b
Jabez F <86470839+DigitalCrood7@users.noreply.github.com>
2022-06-01 20:44:39 +0300

    simple mistake heheh

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,5 @@ for i in range(10):
     for j in range(10):
         if i == 8 and j == 9:
             print("{}{}".format(i, j))
-        elif i != j and j > i:
+        else if i != j and j > i:
             print("{}{}, ".format(i, j), end="")
-            
